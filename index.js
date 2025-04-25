e134d49da07f2e0ee20d328267872400dc7e2606
minazul01 <minajul4010@gmail.com>
2025-04-25 12:43:57 +0600

    get, post, delete added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -29,14 +29,27 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const database = client.db("userDB");
-    const userCollection = database.collection("user");
+    const userCollection = client.db("userDB").collection('user');
+   
+    app.get('/users', async (req, res) => {
+      const cursor = userCollection.find();
+      const result = await cursor.toArray();
+      res.send(result)
+    })
 
     app.post('/users', async (req, res) => {
        const user = req.body;
        console.log('simple user', user)
        const result = await userCollection.insertOne(user);
        res.send(result)
+    })
+
+    app.delete('/users/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log('delete user', id)
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result)
     })
 
     // Send a ping to confirm a successful connection
